31712f23b4015bbcd78facf7d5552f9558b9f2fa
Wunst <matthiesbe@gmail.com>
2024-05-28 10:39:03 +0200

    chore: Update inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { nixpkgs, home-manager, ... }:
